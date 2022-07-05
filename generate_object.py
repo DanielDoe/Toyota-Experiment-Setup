b485ee90b464a846c2ce6ce47f5d725da0a9b8d0
DanielDoe <danieldoe33@gmail.com>
2022-07-04 22:22:17 -0400

    added generate.py

---
+++ generate_object.py
@@ -0,0 +1,19 @@
+import sys, os
+import subprocess
+
+os.system("gnome-terminal -- python /opt/carla-simulator/PythonAPI/examples/automatic_control.py")
+#subprocess.Popen(["gnome-terminal", "-e", "python /opt/carla-simulator/PythonAPI/examples/automatic_control.py"])
+#subprocess.Popen(["gnome-terminal", "-e", "python /opt/carla-simulator/PythonAPI/examples/automatic_control.py"])
+#subprocess.Popen(["gnome-terminal", "-e", "python /opt/carla-simulator/PythonAPI/examples/automatic_control.py"])
+#subprocess.Popen(["gnome-terminal", "-e", "python /opt/carla-simulator/PythonAPI/examples/automatic_control.py"])
+#subprocess.Popen(["gnome-terminal", "-e", "python /opt/carla-simulator/PythonAPI/examples/automatic_control.py"])
+
+# https://medium.com/analytics-vidhya/spawning-vehicles-in-carla-86429f767040
+
+#https://github.com/carla-simulator/carla/issues/764
+
+# https://carla.readthedocs.io/en/latest/core_actors/#vehicles
+
+#https://github.com/carla-simulator/carla/issues/1561
+
+# https://github.com/carla-simulator/carla/issues/127
